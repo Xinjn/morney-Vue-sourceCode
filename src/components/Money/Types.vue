43795dbce03d5c9ff9449a3d3ded73cbda4717ba
Xinjn <422075616@qq.com>
2021-02-16 22:57:23 +0800

    完成 Types 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -9,26 +9,22 @@
             </div>
 </template>
 
-<script>
-export default {
- name:'Types',
- props:['xxx'],
- data(){
-     return {
-         type:'-'
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Type extends Vue{
+ type = '-'
+
+ selectType(type:string){
+     if(type !== '-' && type !== '+'){
+         throw new Error('type is unknon')
      }
- },
- mounted(){
-     console.log(this.xxx)
- },
- methods:{
-     selectType(type){
-         if(type !== '-' && type !== '+'){
-             throw new Error('type is unknown')
-         }
-         this.type = type
-     }
+     this.type = type
  }
+ created(){}
+ mounted(){}
 }
 </script>
 
