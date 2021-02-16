c038eb9c5ed87311bd4c938f6c7274cee4afb946
Xinjn <422075616@qq.com>
2021-02-17 00:26:48 +0800

    添加createdAt字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,11 +16,14 @@ import Notes from "@/components/Money/Notes.vue"
 import Tags from "@/components/Money/Tags.vue"
 import {Component, Watch} from 'vue-property-decorator'
 
+const recordList:Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]')
+
 type Record = {
     tags:string[]
     notes:string
     type:string
     amount:number
+    createdAt?:Date
 }
 
 @Component({
@@ -28,7 +31,7 @@ type Record = {
 })
 export default class Money extends Vue{
     tags = ['衣','食','住','行']
-    recordList:Record[] = []
+    recordList:Record[] = recordList
     record:Record = {
         tags:[],notes:'',type:'-',amount:0
     }
@@ -45,6 +48,7 @@ export default class Money extends Vue{
 
     saveRecord(){
         const record2 = JSON.parse(JSON.stringify(this.record))
+        record2.createdAt = new Date()
         this.recordList.push(record2)
     }
 
