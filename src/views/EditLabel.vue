3cd9a223d541b276cd92b96afa1a5067d62a53b0
Xinjn <422075616@qq.com>
2021-02-18 17:39:01 +0800

    添加EditLabel.vue的样式，抽离了两个组件Button和FormItem

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -4,8 +4,9 @@
             <Icon name="left"/>
             <span>编辑标签</span>
         </div>
-        <Notes fieldName="标签名"
+        <FormItem fieldName="标签名"
                placeholder="请输入标签名"/>
+        <Button>删除标签</Button>
     </Layout>
 </template>
 
@@ -13,11 +14,11 @@
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator'
 import tagListModel from '@/models/tagListModel'
-import Notes from '../components/Money/Notes.vue';
-
+import FormItem from '../components/Money/FormItem.vue';
+import Button from '../components/Money/Button.vue';
 
 @Component({
-    components:{Notes}
+    components:{FormItem,Button}
 })
 export default class EditLabel extends Vue{
     created(){
