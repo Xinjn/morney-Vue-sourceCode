e195fea6f246ce0f36a6c5cd63f128e677d1da7b
Xinjn <422075616@qq.com>
2021-02-21 21:41:45 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,7 +14,6 @@
 <script lang="ts">
 import Vue from 'vue'
 import NumberPad from "@/components/Money/NumberPad.vue"
-import Types from "@/components/Money/Types.vue"
 import FormItem from "@/components/Money/FormItem.vue"
 import Tags from "@/components/Money/Tags.vue"
 import {Component} from 'vue-property-decorator'
@@ -22,7 +21,7 @@ import recordTyedList from '@/constant/recordTyedList';
 import Tabs from "@/components/Tabs.vue"
 
 @Component({
-    components:{NumberPad,Tabs,Types,FormItem,Tags}
+    components:{NumberPad,Tabs,FormItem,Tags}
 })
 export default class Money extends Vue{
     get recordList(){
