24b559a5a5f02be183eba9cd90f91ac44eee809c
Xinjn <422075616@qq.com>
2021-02-22 15:55:12 +0800

    完成列表的CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,6 +2,20 @@
     <Layout>
         <Tabs class-prefix='type' :data-source='recordTyedList' :value.sync="type"/>
         <Tabs class-prefix='interval' :data-source='intervalList' :value.sync="interval"/>
+        <div>
+            <ol>
+                <li v-for="(group,index) in result" :key="index">
+                    <h3 class="title">{{group.title}}</h3>
+                    <ol>
+                        <li v-for="item in group.items" :key="item.id" class="record">
+                        <span>{{tagString(item.tags)}}</span>
+                        <span class="notes">{{item.notes}}</span>
+                        <span>{{item.amount}}</span>
+                        </li>
+                    </ol>
+                </li>
+            </ol>
+        </div>
     </Layout>
 </template>
 
@@ -16,12 +30,63 @@ import recordTyedList from '@/constant/recordTyedList';
     components:{Tabs}
 })
 export default class Statistics extends Vue{
+    tagString(tags:Tag[]){
+        return tags.length === 0 ? '无' : tags.join(',')
+    }
+
+    get recordList(){
+        return (this.$store.state as RootState).recordList
+    }
+    get result() {  
+        const {recordList} = this  
+        type HashTableValue = { title:string,items:RecordItem[] }         
+        const hashTabel:{ [key:string]:HashTableValue } = {} 
+        
+        for(let i = 0;i<recordList.length;i++){
+           
+
+            const [date,time] = recordList[i].createdAt!.split('T') 
+            
+            
+            hashTabel[date] = hashTabel[date] || {title:date,items:[]} 
+            
+            hashTabel[date].items.push(recordList[i]) 
+            
+
+        }
+        return hashTabel 
+    }
+    beforeCreate(){
+        this.$store.commit('fetchRecords')
+    }
     type = '-'
     interval = "day"
     intervalList = intervalList
     recordTyedList = recordTyedList
 }
 </script>
+
+<style lang="scss" scoped>
+  %item {
+      padding: 8px 16px;
+      line-height: 24px;
+      display: flex;
+      justify-content: space-between;
+      align-content: center;
+  }
+  .title {
+      @extend %item
+  }
+  .record {
+      background: white;
+      @extend %item
+  }
+  .notes {
+      margin-right: auto;
+      margin-left: 16px;
+      color:#999;
+  }
+</style>
 
 <style lang="scss" scoped>
   ::v-deep .type-tabs-item{
@@ -32,7 +97,9 @@ export default class Statistics extends Vue{
              display: none;
          }
      }
-     
+ }
+  ::v-deep .interval-tabs-item {
+     height:48px;
  }
 </style>
 
