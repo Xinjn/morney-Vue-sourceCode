e03dd155f2e4c20ae89c49c3f1e4193dd397de17
Xinjn <422075616@qq.com>
2021-03-02 23:54:46 +0800

    构造30天的数据（借鉴计数排序）

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -32,6 +32,8 @@ import recordTyedList from '@/constant/recordTyedList';
 import dayjs from 'dayjs'
 import clone from '@/lib/clone';
 import Chart from '../components/Chart.vue';
+import * as _ from "lodash";
+import day from 'dayjs'
 
 @Component({
     components:{Tabs,Chart}
@@ -67,8 +69,33 @@ export default class Statistics extends Vue{
             return day.format('YYYY年M月D日')
         }
     }
+    get y(){
+        const today = new Date()
+        const array = []
+        for(let i = 0;i <= 29;i++){
+            const dateString = day(today)
+                .subtract(i,'day').format('YYYY-MM-DD')
+            const found = _.find(this.recordList,{createdAt:dateString})
+            array.push({
+                date:dateString,value:found ? found.amount : 0
+            })
+        }
 
+        array.sort((a,b) => {
+            if(a.date > b.date){
+                return 1
+            }else if(a.date === b.date){
+                return 0
+            }else{
+                return -1
+            }
+        })
+        return array
+    }
     get x() {
+        const keys = this.y.map(item => item.date)
+        const values = this.y.map(item => item.value)
+        
         return {
             grid:{
                 left:0,
@@ -77,9 +104,7 @@ export default class Statistics extends Vue{
                 xAxis: {
                     type: 'category',
                     data: [
-                        '1', '2', '3', '4', '5', '6', '7','8', '9', '10',
-                        '11', '12', '13', '14', '15', '16', '17','18', '19', '20',
-                        '21', '22', '23', '24', '25', '26', '27','28', '29', '30',
+                        keys
                         ],
                         axisTick:{alignWithLabel:true},
                         axisLine:{lineStyle:{color:'#666'}}
@@ -92,13 +117,7 @@ export default class Statistics extends Vue{
                     symbol:'circle',
                     symbolSize:12,
                     itemStyle:{borderWidth:1,color:'#666',borderColor:'#666'},
-                    data: [
-                        150, 230, 224, 218, 135, 147, 260,
-                        150, 230, 224, 218, 135, 147, 260,
-                        150, 230, 224, 218, 135, 147, 260,
-                        150, 230, 224, 218, 135, 147, 260,
-                        147, 260,
-                        ],
+                    data:values,
                     type: 'line'
                 }],
                 tooltip:{
